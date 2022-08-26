9ba85233663750fd64de3cac7c1e00cb9d86ab7b
psychedetol <535101602@qq.com>
2022-08-26 17:59:15 +0400

    feat: whitepaper

--- src/frontend/view/Intro.tsx
+++ src/frontend/view/Intro.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import Card from "../component/Card"
 import { Footer } from "../component/Footer"
-import Card  from "../component/Card"
-import { Auth } from "../component/Auth"
 // import { counter } from "canisters/counter"
 import logo from "../assets/nnsdao-logo-1024.png"
 
@@ -10,7 +9,7 @@ export function Intro() {
 
   // const refreshCounter = async () => {
   //   const res = await counter.read()
-    // setCount(res.toString())
+  // setCount(res.toString())
   //   console.log(res.toString())
   // }
 
@@ -24,36 +23,59 @@ export function Intro() {
   // }
 
   const linkList = [
-    { id: 'https://nnsdao.org/', name: "NnsDAO Website" },
-    { id: 'https://sznps-4aaaa-aaaah-qab2a-cai.ic0.app/', name: "Icpscan" },
-    { id: 'https://lm5fh-ayaaa-aaaah-aafua-cai.ic0.app/', name: "ICTexas" },
-    { id: 'https://dscvr.ic0.app/user/nnsdao', name: "Dscvr" },
-    { id: 'https://nnsdao.substack.com/', name: "Substack" },
-    { id: 'https://az5sd-cqaaa-aaaae-aaarq-cai.ic0.app/u/nnsdaos', name: "Distrikt" },
-    { id: 'https://t.me/NnsDaos', name: "Telegram" },
-    { id: 'https://twitter.com/NnsDaos', name: "Twitter" },
-    { id: 'https://github.com/NnsDao/nnsdao-org/blob/main/static/NnsDAO_Boundaryless_Autonomous_Organization.pdf', name: "White Paper" },
-  ];
+    { id: "https://nnsdao.org/", name: "NnsDAO Website" },
+    { id: "https://twitter.com/NnsDaos", name: "Twitter" },
+    {
+      id: "https://h637e-ziaaa-aaaaj-aaeaa-cai.raw.ic0.app/",
+      name: "Nomos Platform",
+    },
+    { id: "https://sznps-4aaaa-aaaah-qab2a-cai.ic0.app/", name: "Icpscan" },
+    { id: "https://icmarket.app/", name: "ICmarket" },
+    { id: "https://lm5fh-ayaaa-aaaah-aafua-cai.ic0.app/", name: "ICTexas" },
+    { id: "https://t.me/NnsDaos", name: "Telegram" },
+    { id: "https://discord.gg/UTZvcDkeDr", name: "Discord" },
+    {
+      id: "https://github.com/NnsDao/nnsdao-org/blob/main/static/NnsDAO_Boundaryless_Autonomous_Organization.pdf",
+      name: "White Paper",
+    },
+    {
+      id: "https://syunduel.medium.com/nnsdao-whitepaper-japanese-translation-922c47e3840b",
+      name: "White Paper Japanese",
+    },
+    {
+      id: "https://github.com/demianDAO/internetcomputer.guide/blob/main/static/whitepaper/NnsDAO_Boundaryless_Autonomous_Organization_kkdemian.pdf",
+      name: "White Paper Chinese",
+    },
+    {
+      id: "https://github.com/NnsDao/nnsdao-org/blob/main/static/NnsDAO_DAOs_To_Earn.pdf",
+      name: "Contribution to earn",
+    },
+  ]
 
   return (
     <>
       <div className="relative  pb-20 font-mono text-cyan-900">
-
         <div className="w-full bg-white rounded-lg p-5 flex flex-col justify-center items-center">
-            <div className="mb-8">
-                <img className="object-center object-cover rounded-full h-36 w-36" src={logo} alt="photo" />
-                <p className="text-center mt-2" >@NnsDAO</p>
-            </div>
-            <div className="text-center">
-                <p className="text-base text-gray-400 font-normal" >A link to your Web 3.0 application.</p>
-            </div>
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src={logo}
+              alt="photo"
+            />
+            <p className="text-center mt-2">@NnsDAO</p>
+          </div>
+          <div className="text-center">
+            <p className="text-base text-gray-400 font-normal">
+              A link to your Web 3.0 application.
+            </p>
+          </div>
         </div>
 
         <div className="flex justify-center py-5">
           <div className="container max-w-3xl p-4">
-              {linkList.map((link) => (
-                <Card name={link.name} url={link.id}  key={link.id} />
-              ))}
+            {linkList.map((link) => (
+              <Card name={link.name} url={link.id} key={link.id} />
+            ))}
           </div>
         </div>
         <Footer />
